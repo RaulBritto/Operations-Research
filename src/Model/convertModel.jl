c974ad538dd0a76a0cde2043822e979a05b5256b
Raul Britto <raulbbritto@gmail.com>
2020-05-26 03:32:17 -0300

    Adding variables constraints <=

--- src/Model/convertModel.jl
+++ src/Model/convertModel.jl
@@ -1,6 +1,9 @@
 function Model2Matriz(model::Model)
 
     c = Array{Float64,1}()
+    b = Array{Float64,1}()
+    A = Array{Float64}(undef, 0, num_variables(model))
+    restriction = reshape(zeros(num_variables(model)), 1, num_variables(model))
 
     min_max = Int8(objective_sense(model))
 
@@ -15,8 +18,28 @@ function Model2Matriz(model::Model)
        append!(c,objective.terms[variables[i]])
     end
 
-    println(c)    
-    
 
+    for i=1:num_variables(model)
+
+        if has_upper_bound(variables[i])
+            append!(b,upper_bound(variables[i]))
+        end
+
+        if has_lower_bound(variables[i])
+            if lower_bound(variables[i]) == 0
+                restriction[i] = 1
+                A = vcat(A,restriction)
+                restriction[i] = 0
+            else
+                #TODO new lower_bound to 0
+            end
+        else
+            #TODO create lower_bound
+        end
+    end
+    
+    println(A)
+    println(b)
+    println(c)
 
 end
