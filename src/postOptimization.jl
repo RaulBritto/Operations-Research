5340b16d4ecf6f3a12cca805e296aef4cd936527
Raul Britto <raulbbritto@gmail.com>
2020-06-11 18:03:13 -0300

    Adding postOptimization to constraits

--- src/postOptimization.jl
+++ src/postOptimization.jl
@@ -3,11 +3,31 @@ function SensAnalysis(tableau, model::Model)
     shadowPrice = tableau[1,num_variables(model)+1:num_variables(model)+nrows]
     
     S = tableau[2:end,num_variables(model)+1:num_variables(model)+nrows]
+    b_ = tableau[2:end, end]
 
-    println("\nPreço sombra:")
+    println("\nDual:")
     for i = 1: length(shadowPrice)
         println("restrição[", i, "] = ", shadowPrice[i])
     end
 
+    v = Float64[-Inf, Inf]
+    V = Array[]    
+    for i = 1:nrows
+        push!(V,copy(v))
+        delta = zeros(nrows)
+        delta[i] = 1
+        x = -b_./(S*delta)
+        for j = 1:nrows
+            if S[j,i] > 0 && V[i][1] < x[j]
+                V[i][1] = x[j]
+            elseif S[j,i] < 0 && V[i][2] > x[j]
+                V[i][2] = x[j]
+            end
+        end
+        replace!(V[i], Inf=>0)
+        replace!(V[i], -Inf=>0)
+
+        println(b[i]-V[i][1]" <= b[",i,"] <= ", b[i]+V[i][2])
+    end
     
 end
